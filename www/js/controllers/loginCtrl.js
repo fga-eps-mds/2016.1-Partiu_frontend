d5d0d92e4175a8ac2f6a241227e1fbce36bd7f97
Dylan Guedes <djmgguedes@gmail.com>
2016-04-23 18:00:08 -0300

    Improves the fb login, and enhance the app architecture

--- www/js/controllers/loginCtrl.js
+++ www/js/controllers/loginCtrl.js
@@ -2,17 +2,14 @@ angular.module('starter.controllers')
 
 .controller('loginCtrl', function($scope, $ionicModal, $timeout, ngFB) {
   $scope.fbLogin = function () {
-    ngFB.login({scope: 'email, publish_actions'}).then(
-        function (response) {
-            if (response.status === 'connected') {
-                console.log('Facebook login succeeded');
-                /*$scope.closeLogin();*/
-                console.log(response);
-            } else {
-                alert('Facebook login failed');
-            }
-        });
-	};
+    ngFB.login({scope: 'email, publish_actions'}).then(function(response) {
+      if (response.status === 'connected') {
+        console.log('Facebook login succeeded');
+        console.log(response);
+      } else {
+        alert('Facebook login failed');
+      }
+    });
+  };
 })
 
-/*read_stream,*/
