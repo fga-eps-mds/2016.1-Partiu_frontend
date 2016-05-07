a1083f8fde8e8bafa8d559fdb7d6c6d321b5cd3d
eduqg <eduardoqgomes@gmail.com>
2016-05-07 10:33:18 -0300

    Changing rideCtrl

--- www/js/controllers/rideCtrl.js
+++ www/js/controllers/rideCtrl.js
@@ -15,15 +15,17 @@ origin destiny is_only_departure total_seats departure_time return_time is_finis
 */
 .controller('rideCtrl', function ($scope){
    $scope.rides = [
-       {nameDriver: 'Daniel', numberSeats: 4, img : "http://pink.dornbeast.com/wp-content/uploads/2008/07/7803325b0b54_main400-300x300.jpg",
+       {nameDriver: 'Daniel', numberSeats: 4,
+        img : "http://pink.dornbeast.com/wp-content/uploads/2008/07/7803325b0b54_main400-300x300.jpg",
         origin: 'Gama', destiny:'Asa Norte', is_only_departure: 'Sim',
-        departure_time: '10:00', return_time:'12:00', is_finished: 'No',
+        departure_time: '10:00', return_time:'12:00', is_finished: 'Não',
         is_subsistence_allowance: 'Não', description: 'Bora Galera!',
         rank: 'Confiável'
        },
-       {nameDriver: 'Eduardo', numberSeats: 4, img : "http://pink.dornbeast.com/wp-content/uploads/2008/07/7803734ee134_main400-300x300.jpg",
+       {nameDriver: 'Eduardo', numberSeats: 4,
+        img : "http://pink.dornbeast.com/wp-content/uploads/2008/07/7803734ee134_main400-300x300.jpg",
         origin: 'Taguatinga', destiny:'Plano Piloto', is_only_departure: 'Sim',
-        departure_time: '14:00', return_time:'19:00', is_finished: 'No',
+        departure_time: '14:00', return_time:'19:00', is_finished: 'Não',
         is_subsistence_allowance: 'Sim', description: 'Partiu!',
         rank: 'Confiável'
        },
