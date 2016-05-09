9a7b815df23e3d6ab1d8d17089ab56e98e348e7f
Victor Arnaud <victor_hugo_ad@hotmail.com>
2016-05-09 16:21:12 -0300

    Form layout finished and ride controller almost done

--- www/js/controllers/rideCtrl.js
+++ www/js/controllers/rideCtrl.js
@@ -1,24 +1,60 @@
 angular.module('starter.controllers')
 
-.controller('rideCtrl', function($scope, RideAPI, $http) {
+.controller('rideCtrl', function($scope, RideAPI, VehicleAPI, RegisterRide, $http) {
   $scope.rides = [];
+  $scope.vehicles = [];
+  $scope.ride = {};
+  $scope.vehicle = {};
+  $scope.message = '';
+  $scope.filtro = '';
+
+  /*if($routeParams.rideID) {
+      RideAPI.get({rideId: $routeParams.rideID}, function(ride) {
+          $scope.ride = ride;
+      }, function(erro) {
+          console.log(erro.status);
+          $scope.message = 'Não foi possível obter a carona'
+      });
+  }*/
 
   RideAPI.query().$promise.then(function(response){
     $scope.rides = response;
-    console.log($scope.rides)
+    console.log($scope.rides);
   });
 
-  $scope.submitRide = function(newRide) {
-    console.log("ride:");
-    console.log(newRide);
-    $http.post('http://localhost:3000/api/rides', newRide).success(function(data) {
-      console.log("data_ride:");
-      console.log(data);
-    });
-    $http.post('http://localhost:3000/api/vehicles', newRide).success(function(data) {
-      console.log("data_vehicle");
-      console.log(data);
-    })
-  }
+  VehicleAPI.query().$promise.then(function(response){
+    $scope.vehicles = response;
+    console.log($scope.vehicles);
+  });
 
+  $scope.remove = function(ride) {
+		RideAPI.delete({rideID: ride.id}, function(){
+			var rideIndex = $scope.rides.indexOf(ride);
+			$scope.rides.splice(rideIndex, 1);
+			$scope.message = "Carona " + ride.title + " foi removida com sucesso!";
+		}, function(erro){
+			console.log(erro.status);
+			$scope.message = "Não foi possivel remover a carona " + ride.title;
+		});
+	};
+
+  $scope.submitRide = function() {
+    //  if($scope.form.$valid) {
+       console.log("ride:");
+       console.log($scope.ride);
+       console.log($scope.vehicle);
+
+       RegisterRide.register($scope.ride, $scope.vehicle)
+       .then(function(data_success){
+         $scope.message = data_success.message;
+         console.log($scope.message);
+         if(data_success.create) $scope.ride = {};
+       })
+       .catch(function(data_error){
+         $scope.message = data_error.message;
+         console.log($scope.message);
+       });
+
+    // }
+  };
 });
