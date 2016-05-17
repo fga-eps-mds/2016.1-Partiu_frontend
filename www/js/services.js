8e3bc555b214fb893225e5c086a1059122d82144
João Pedro Sconetto <sconetto.joao@gmail.com>
2016-05-17 19:33:29 -0300

    Removed the profile function and added in the user service

--- www/js/services.js
+++ www/js/services.js
@@ -1,28 +1,5 @@
 angular.module('starter.services', [])
 
-.service('Profile', function() {
-  var user = {}
-
-  var setUser = function(name, email, token, gender, photoURL, userId, profileLink) {
-    user.name = name,
-    user.email = email,
-    user.token = token,
-    user.gender = gender,
-    user.photo = photoURL,
-    user.id = userId,
-    user.facebook_profile = profileLink
-  }
-
-  var getUser = function() {
-    return user;
-  }
-
-  return {
-    setUser: setUser,
-    getUser: getUser
-  }
-})
-
 .factory('RideAPI', function($resource) {
   return $resource("http://104.236.252.208/api/users/1/rides/:rideID", null , {
     update: {
