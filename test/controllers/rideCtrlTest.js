4382f09706b5d45877b98644c6fce229693a6f2d
DylanGuedes <djmgguedes@gmail.com>
2016-05-26 20:55:03 -0300

    Start tests for the rideCtrl, fixes angular config

--- test/controllers/rideCtrlTest.js
+++ test/controllers/rideCtrlTest.js
@@ -1,10 +1,25 @@
 describe('rideCtrl tests', function() {
+  var newCtrl;
+
   describe('configuration', function() {
-    it(' - test #1', function($controller) {
-      var controller = $controller('rideCtrl', {});
-      expect(controller).toBeDefined();
-      expect(controller.rides).toBe([]);
-      expect(controller.rides).toBeDefined();
+    beforeEach(function() {
+      module('starter');
+      module('starter.controllers');
+      module('starter.services');
     });
+
+    it('should be defined', inject(function($controller) {
+      var scope = {};
+      var controller = $controller('rideCtrl', {
+        $scope: scope,
+        RegisterRide: null,
+        $http: null
+      });
+
+      expect(controller).toBeDefined();
+      expect(scope).toBeDefined();
+      expect(scope.remove).toBeDefined();
+      expect(scope.rides).toBeDefined();
+    }));
   });
 });
