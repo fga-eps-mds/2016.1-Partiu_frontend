481b11c9772a785a97f87dd22b65c874019c0af9
eduqg <eduardoqgomes@gmail.com>
2016-05-27 19:18:46 -0300

    aboutCtrl describe correction

--- test/controllers/aboutCtrlTests.js
+++ test/controllers/aboutCtrlTests.js
@@ -1,31 +1,28 @@
-describe('rideCtrl tests', function() {
+describe('aboutCtrl tests', function(){
   describe('instantiation and scope tests', function() {
-    var $httpBackend, $rootScope, createController, authHandler;
+    var $httpBackend, $rootScope, createController;
     beforeEach(function() {
       module('starter');
       module('starter.controllers');
       module('starter.services');
     });
-
     beforeEach(inject(function($injector) {
       var $controller = $injector.get('$controller');
       $rootScope = $injector.get('$rootScope');
-
       createController = function() {
-        return $controller('rideCtrl', {'$scope': $rootScope});
+        return $controller('aboutCtrl', {'$scope': $rootScope});
       };
     }));
-
     it('should be defined and initializes', (function() {
       var controller = createController();
       expect(controller).toBeDefined();
       expect($rootScope).toBeDefined();
-      expect($rootScope.remove).toBeDefined();
-      expect($rootScope.rides).toBeDefined();
-      expect($rootScope.rides).toEqual([]);
-      expect($rootScope.message).toEqual('');
+      expect($rootScope.user).toBeDefined();
+
+      console.log($rootScope.user);
+
     }));
   });
 
 
-});
+})
