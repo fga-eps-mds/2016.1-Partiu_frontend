2cfc54ebb4b11605cd2b43a4410d231d58dccc99
eduqg <eduardoqgomes@gmail.com>
2016-06-13 14:12:51 -0300

    Adding all files to tests and initializing them

--- test/controllers/menuCtrlTest.js
+++ test/controllers/menuCtrlTest.js
@@ -1,6 +1,5 @@
-describe('mapCtrl tests', function() {
+describe('menuCtrl tests', function() {
   describe('instantiation and scope tests', function() {
-
     var $httpBackend, $rootScope, createController;
     beforeEach(function() {
       module('starter');
@@ -9,21 +8,18 @@ describe('mapCtrl tests', function() {
     });
 
     beforeEach(inject(function($injector) {
-
       var $controller = $injector.get('$controller');
       $rootScope = $injector.get('$rootScope');
       createController = function() {
-        return $controller('mapCtrl', {'$scope': $rootScope});
+        return $controller('menuCtrl', {'$scope': $rootScope});
       };
     }));
 
     it('should be defined and initialized', (function() {
-
       var controller = createController();
       expect(controller).toBeDefined();
       expect($rootScope).toBeDefined();
     }));
   });
-  
 });
 
