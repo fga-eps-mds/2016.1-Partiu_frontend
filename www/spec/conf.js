7badf155c4aedf21d4450c782f5142ba10515224
danmoura17 <danmoura17@gmail.com>
2016-06-16 21:25:33 -0300

    Changing funcionality test name

--- www/spec/conf.js
+++ www/spec/conf.js
@@ -2,8 +2,8 @@ exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: [
     //'facebookLoginSpec.js',
-    //'createRideSpec.js'
-    'showRideSpec.js'
+    'createRideSpec.js'
+    //'showRideSpec.js'
   ],
   jasmineNodeOpts: {
     showColors: true,
