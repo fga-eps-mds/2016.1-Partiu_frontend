19ee7708a8e17f1ddb56eeba2605053c9c51d174
DylanGuedes <djmgguedes@gmail.com>
2016-06-18 22:40:28 -0300

    finished tests for rideCtrl

--- www/js/controllers/rideCtrl.js
+++ www/js/controllers/rideCtrl.js
@@ -36,8 +36,8 @@ angular.module('starter.controllers')
       $scope.message = "Carona " + $scope.ride.title + " incluída com sucesso";
       $state.go('menu.showRide', {"id": response.id});
     }, function(erro) {
-      console.error("Não foi possível incluír a carona " + $scope.ride.title);
-      $scope.message = "Não foi possível incluír a carona " + $scope.ride.title;
+      console.error("Não foi possível incluir a carona " + $scope.ride.title);
+      $scope.message = "Não foi possível incluir a carona " + $scope.ride.title;
       console.error(erro);
     });
   };
@@ -56,86 +56,80 @@ angular.module('starter.controllers')
   $scope.showExitButton = false
 
   $scope.loadRide = function() {
-    $scope.loadRide = RideAPI.rides.get({rideId: $stateParams.id}).$promise
-    .then(function(response) {
-      $scope.ride = response;
-      var user_id = Profile.getUser().backendId;
-      var driver_id = $scope.ride.driver.user_id;
+    RideAPI.rides.get({rideId: $stateParams.id, userId: Profile.getUser().backendId}).$promise
+      .then(function(response) {
+        $scope.ride = response;
+        var user_id = Profile.getUser().backendId;
+        if ($scope.ride.driver) {
+          var driver_id = $scope.ride.driver.user_id;
 
-      UserAPI.query().$promise.then(function(response) {
-        $scope.users = response;
-        for(i=0; i<$scope.users.length; i++) {
-          if($scope.users[i].id == user_id) {
-            $scope.current_user = $scope.users[i]
-          }
+          UserAPI.query().$promise.then(function(response) {
+            $scope.users = response;
+            console.log("users:", $scope.users);
+            for(i=0; i<$scope.users.length; i++) {
+              if($scope.users[i].id == user_id) {
+                $scope.current_user = $scope.users[i]
+              }
 
-          if($scope.users[i].id == driver_id){
-            $scope.user_driver = $scope.users[i]
-            $scope.driver_vehicles = $scope.user_driver.driver.vehicles
-          }
-        }
-
-        for(i=0; i<$scope.driver_vehicles.length; i++) {
-          if($scope.driver_vehicles[i].id == $scope.ride.vehicle_id)
-            $scope.vehicle_ride = $scope.driver_vehicles[i]
-        }
-
-        for(i=0; i<=$scope.ride.passengers; i++) {
-          if($scope.ride.passengers[i] == $scope.current_user.id && $scope.current_user.driver.id != $scope.ride.driver.id && $scope.ride.total_seats < 4)
-            $scope.showExitButton = true
-          else if($scope.ride.passengers[i] != $scope.current_user.id && $scope.current_user.driver.id != $scope.ride.driver.id && $scope.ride.total_seats > 0)
-            $scope.showEnterButton = true
-          else
-            $scope.showEditRide = true
-        }
-
-        $scope.imgs.splice(0, 4-$scope.ride.total_seats + $scope.ride.passengers.length)
-
-        $scope.enterRide = function() {
-          if($scope.ride.total_seats > 0 && $scope.current_user.id != $scope.ride.driver.id) {
-            console.log("inserindo usuario ", $scope.current_user.id);
-            console.log("na carona ", $scope.ride.title);
-            $http.post(AppSettings.baseApiUrl + '/api/insert_passenger', {"ride_id": $scope.ride.id, "passenger_id": $scope.current_user.passenger.id}).success(function(response) {
-              console.log(response);
-            });
-            $scope.ride.passengers.push($scope.current_user.passenger)
-            console.log($scope.ride.passengers)
-            $scope.ride.total_seats -= 1
-            RideAPI.userRides.update({userId: $scope.user_driver.id, rideId: $scope.ride.id}, {ride: $scope.ride}).$promise
-            .then(function(response) {
-              console.log("Passageiro incluido com sucesso");
-              $state.go('menu.rides');
-            }, function(erro) {
-              console.error("Não foi possível inserir o passageiro");
-              console.error(erro);
-            });
-          }
-        }
-
-        $scope.exitRide = function() {
-          if($scope.ride.total_seats < 4) {
-            for(i=0; i<$scope.ride.passengers; i++) {
-              if($scope.ride.passengers[i] == $scope.current_user.passenger.id)
-                $scope.ride.passengers.splice(i, 1)
+              if($scope.users[i].id == driver_id){
+                $scope.user_driver = $scope.users[i]
+                  $scope.driver_vehicles = $scope.user_driver.driver.vehicles
+              }
             }
-            $scope.ride.total_seats += 1;
-            RideAPI.userRides.update({userId: $scope.user_driver.id, rideId: $scope.ride.id}, {ride: $scope.ride}).$promise
-              .then(function(response) {
-                console.log("Passageiro incluido com sucesso");
-                $state.go('menu.rides');
-              }, function(erro) {
-                console.error("Não foi possível inserir o passageiro");
-                console.error(erro);
-              });
-            }
-        }
 
-      });
+            if ($scope.driver_vehicles) {
+              for(i=0; i<$scope.driver_vehicles.length; i++) {
+                if($scope.driver_vehicles[i].id == $scope.ride.vehicle_id)
+                  $scope.vehicle_ride = $scope.driver_vehicles[i]
+              }
+            }
+
+            if ($scope.ride.passengers) {
+              for(i=0; i<=$scope.ride.passengers; i++) {
+                if($scope.ride.passengers[i] == $scope.current_user.id && $scope.current_user.driver.id != $scope.ride.driver.id && $scope.ride.total_seats < 4)
+                  $scope.showExitButton = true
+                else if($scope.ride.passengers[i] != $scope.current_user.id && $scope.current_user.driver.id != $scope.ride.driver.id && $scope.ride.total_seats > 0)
+                  $scope.showEnterButton = true
+                else
+                  $scope.showEditRide = true
+              }
+              $scope.imgs.splice(0, 4-$scope.ride.total_seats + $scope.ride.passengers.length)
+            }
+
+
+              $scope.enterRide = function() {
+                if($scope.ride.total_seats > 0 && $scope.current_user.id != $scope.ride.driver.id) {
+                  console.log("inserindo usuario ", $scope.current_user.id);
+                  console.log("na carona ", $scope.ride.title);
+                  $http.post(AppSettings.baseApiUrl + '/api/insert_passenger', {"ride_id": $scope.ride.id, "passenger_id": $scope.current_user.passenger.id}).success(function(response) {
+                    console.log(response);
+                  });
+                  $scope.ride.passengers.push($scope.current_user.passenger);
+                  $scope.ride.total_seats -= 1
+                }
+              }
+
+            $scope.exitRide = function() {
+              console.log("TOTAL SEATS:", $scope.ride.total_seats);
+              if($scope.ride.total_seats < 4) {
+                for(i=0; i<$scope.ride.passengers; i++) {
+                  if($scope.ride.passengers[i] == $scope.current_user.passenger.id)
+                    $scope.ride.passengers.splice(i, 1)
+                }
+                $scope.ride.total_seats += 1;
+                RideAPI.userRides.update({userId: $scope.user_driver.id, rideId: $scope.ride.id}, {ride: $scope.ride}).$promise.then(function(response) {
+                    console.log("Passageiro incluido com sucesso");
+                    $state.go('menu.rides');
+                  });
+              }
+            }
+
+          });
+      }
     }, function(erro) {
       $scope.message = "Não foi possivel encontrar a carona " + $stateParams.id;
     });
-  }
-
+  };
   $scope.loadRide();
 })
 
