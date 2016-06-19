c59568d306395dd9974d5ec9687b07fe62aa5cda
eduqg <eduardoqgomes@gmail.com>
2016-06-19 14:33:37 -0300

    Continuing tests of menuCtrl

--- test/controllers/menuCtrlTest.js
+++ test/controllers/menuCtrlTest.js
@@ -25,70 +25,41 @@ describe('menuCtrl tests', function() {
 
 
 
-    it('should be defined if the user is logged',(function() {
+    it('should know if has been canceled Redirect',(function() {
+      var controller = createController();
+
+      expect(controller).toBeDefined();
+      expect($rootScope).toBeDefined();
+      expect($rootScope.cancelRedirect).toBeDefined();
+
+      spyOn($rootScope,'cancelRedirect');
+      $rootScope.cancelRedirect();
+      
+      expect($rootScope.cancelRedirect).toHaveBeenCalled();
+
+
+    }));
+
+
+    it('should close app',(function() {
+      var controller = createController();
+
+      expect(controller).toBeDefined();
+      expect($rootScope).toBeDefined();
+      expect($rootScope.closeApp).toBeDefined();   
+
+      spyOn($rootScope,'closeApp');
+      $rootScope.closeApp();
+      expect($rootScope.closeApp).toHaveBeenCalled( );
+      expect($rootScope.closeApp).toHaveBeenCalledWith( );
+
+      spyOn(ionic.Platform,'exitApp');
+      ionic.Platform.exitApp();
+      expect(ionic.Platform.exitApp).toHaveBeenCalled();
+      expect(ionic.Platform.exitApp).toHaveBeenCalledWith();
       
     }));
 
 
-    it('should close app',(function() {
-      /*var controller = createController();
-      var $controller = $injector.get('$controller');
-      $rootScope = $injector.get('$rootScope');
-
-      expect($rootScope.closeApp(true)).toBe(true);*/
-
-    }));
-
-
   });
 });
-
-    /*
-    it('should close app',(function() {
-      var controller = createController();
-
-      expect($rootScope.closeApp).toBe(false);
-      $rootScope.closeApp;
-      spyOn($rootScope, 'closeApp');
-      expect($rootScope.closeApp()).toHaveBeenCalled();
-    }));
-
-    it("contains spec with an expectation", function() {
-      var controller = createController();
-      $rootScope.closeApp(true).toBe(true);
-    });
-
-
-
-
-$scope.open_login_modal = function() 
-{
-    var temp = $ionicModal.fromTemplateUrl('templates/login.html',{scope: $scope});
-
-    temp.then(function(modal) { 
-        $scope.modal_login = modal;
-        $scope.modal_login.show();
-    });
-};
-
-$scope.close_login_modal = function() 
-{
-    $scope.modal_login.hide();
-};
-
-
-describe('Modal tests', function() {
-  beforeEach(function(){
-    $scope.open_login_modal();
-  });
-  it('should open login modal', function() {
-    expect($ionicModal.fromTemplateUrl).toHaveBeenCalled();
-    expect($ionicModal.fromTemplateUrl.calls.count()).toBe(1);
-  });
-  it('should close login modal', function() {
-    $scope.close_login_modal();
-    spyOn($scope.modal_login, 'hide');
-    expect($scope.modal_login.hide()).toHaveBeenCalled();
-  });
-});
-    */
