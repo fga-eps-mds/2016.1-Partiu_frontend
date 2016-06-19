e06bfc4ab4e9d3851069ae79dc157b9c00e3446e
João Pedro Sconetto <sconetto.joao@gmail.com>
2016-06-19 20:24:55 -0300

    Adding in app open link

--- www/js/controllers/aboutCtrl.js
+++ www/js/controllers/aboutCtrl.js
@@ -7,4 +7,17 @@ angular.module('starter.controllers')
   /*Profile function to get the user from the service*/
   $scope.user = Profile.getUser();
 
+  $scope.inAppOpenLink = function(url) {
+    if (!url) {
+      return
+    }
+
+    $scope.getAvailabilityScheme(url, function(url) {
+      document.addEventListener("deviceready", onDeviceReady, false);
+      function onDeviceReady() {
+        window.open = cordova.InAppBrowser.open(url, '_system', 'location=yes');
+      }
+    });
+  }
+
 })
